1d7765d94acc18670bc17976912ba92f0fa9bb28
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2008-06-25 06:55:41 +0000

    Remove debug output, coding style fixes.

--- plugins/lua/lua.c
+++ plugins/lua/lua.c
@@ -91,28 +91,35 @@ static int add_lua_ruleset(lua_plugin_t *plugin, const char *function)
 }
 
 
-static int timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
-     {
-       /* Perform the carry for the later subtraction by updating y. */
-       if (x->tv_usec < y->tv_usec) {
-         int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-         y->tv_usec -= 1000000 * nsec;
-         y->tv_sec += nsec;
-       }
-       if (x->tv_usec - y->tv_usec > 1000000) {
-         int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-         y->tv_usec += 1000000 * nsec;
-         y->tv_sec -= nsec;
-       }
+static int timeval_subtract(struct timeval *result, struct timeval *x, struct timeval *y)
+{
+        int nsec;
 
-       /* Compute the time remaining to wait.
-          tv_usec is certainly positive. */
-       result->tv_sec = x->tv_sec - y->tv_sec;
-       result->tv_usec = x->tv_usec - y->tv_usec;
+        /*
+         * Perform the carry for the later subtraction by updating y.
+         */
+        if (x->tv_usec < y->tv_usec) {
+                nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+                y->tv_usec -= 1000000 * nsec;
+                y->tv_sec += nsec;
+        }
 
-       /* Return 1 if result is negative. */
-       return x->tv_sec < y->tv_sec;
-     }
+        if (x->tv_usec - y->tv_usec > 1000000) {
+                nsec = (x->tv_usec - y->tv_usec) / 1000000;
+                y->tv_usec += 1000000 * nsec;
+                y->tv_sec -= nsec;
+        }
+
+        /*
+         * Compute the time remaining to wait. tv_usec is certainly positive.
+         */
+        result->tv_sec = x->tv_sec - y->tv_sec;
+        result->tv_usec = x->tv_usec - y->tv_usec;
+
+        /* Return 1 if result is negative. */
+        return x->tv_sec < y->tv_sec;
+}
+
 
 static void lua_run(prelude_plugin_instance_t *pi, idmef_message_t *idmef)
 {
@@ -144,12 +151,9 @@ static void lua_run(prelude_plugin_instance_t *pi, idmef_message_t *idmef)
                 total += result.tv_sec + result.tv_usec * 1e-6;
 
                 lr->elapsed += result.tv_sec + result.tv_usec * 1e-6;
-                printf("[%s]: %f\n", lr->function, result.tv_sec + result.tv_usec * 1e-6);
         }
 
         timeval_subtract(&result, &te, &ts);
-        printf("RAN LUA CODE: %f sec\n", total);
-
         lua_gc(plugin->lstate, LUA_GCCOLLECT, 0);
 }
 

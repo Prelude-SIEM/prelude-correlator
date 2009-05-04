999ae1f75807a788d3edef182a58608fe569226b
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2009-05-04 15:51:40 +0200

    Remove everything in preparation for the import of the Python Correlator version.

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#GNULIB_TOOL=`which gnulib-tool 2> /dev/null`
-#if test -n "$GNULIB_TOOL"; then
-#	echo "Running gnulib-tool..."
-#        $GNULIB_TOOL --import > /dev/null
-#fi
-
-autoreconf --install --force
-echo
-echo "You can now run \"./configure --enable-developer-mode\" and \"make\""

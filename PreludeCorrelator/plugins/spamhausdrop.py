547a9b9cda2cb7d82436b280a2da4ff32d482d55
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2009-12-17 11:02:17 +0100

    Fix SpamhausDrop context creation

--- PreludeCorrelator/plugins/spamhausdrop.py
+++ PreludeCorrelator/plugins/spamhausdrop.py
@@ -108,7 +108,7 @@ class SpamhausDropPlugin(Plugin):
     def run(self, idmef):
         for source in idmef.Get("alert.source(*).node.address(*).address"):
             if IPAddress(source) in self.__mynets:
-                ca = context.Context("SPAMHAUS_" + source, { "expire": 300, "alert_on_expire": True }, update = True, idmef = idmef)
+                ca = Context("SPAMHAUS_" + source, { "expire": 300, "alert_on_expire": True }, update = True, idmef = idmef)
                 if ca.getUpdateCount() == 0:
                         ca.Set("alert.classification.text", "IP source matching Spamhaus DROP dataset")
                         ca.Set("alert.correlation_alert.name", "IP source matching Spamhaus DROP dataset")

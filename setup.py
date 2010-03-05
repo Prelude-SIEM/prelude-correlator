287448ad663f8fc0911636c04f6fd7145711676f
Yoann Vandoorselaere <yoann.v@prelude-technologies.com>
2010-03-05 17:45:57 +0100

    Update NEWS, bump version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from setuptools.command.sdist import sdist
 
-PRELUDE_CORRELATOR_VERSION = "1.0.0rc2"
+PRELUDE_CORRELATOR_VERSION = "1.0.0rc3"
 
 
 class my_sdist(sdist):

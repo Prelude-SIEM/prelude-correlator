dd72bd893abfe880c297d4cae38803c22083f193
Antoine LUONG <antoine.luong@c-s.fr>
2016-02-26 11:30:09 +0100

    Update NEWS, bump version
    Change-Id: I0540ca9602a9fe8ea40f306d668af609df33723c

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from setuptools.command.sdist import sdist
 
-PRELUDE_CORRELATOR_VERSION = "1.3.0alpha1"
+PRELUDE_CORRELATOR_VERSION = "1.3.0beta1"
 
 
 class my_sdist(sdist):

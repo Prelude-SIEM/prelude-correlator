b1cbb05965b0172ccb95c6d29070d91ca4e5fd2d
Antoine LUONG <antoine.luong@c-s.fr>
2016-07-20 14:50:27 +0200

    [#1704] Prevent potential import errors when loading contexts
    If a plugin was deleted but the context.dat still had references of it,
    the unpickling would fail and prevent the correlator from starting.
    
    Change-Id: I3d7e2d073268cc0332024a091771f6e449ba4bdd

--- preludecorrelator/context.py
+++ preludecorrelator/context.py
@@ -331,6 +331,20 @@ def search(name, idmef=None, update=False):
 
     return None
 
+
+class _Dummy:
+    pass
+
+
+class ContextUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        try:
+            return pickle.Unpickler.find_class(self, module, name)
+        except (ImportError, AttributeError) as e:
+            logger.warning(e)
+            return _Dummy
+
+
 def save(profile):
     ctxt_filename = require.get_data_filename("context.dat", profile=profile)
 
@@ -347,7 +361,7 @@ def load(profile):
         fd = open(ctxt_filename, "rb")
 
         try:
-            _CONTEXT_TABLE.update(pickle.load(fd))
+            _CONTEXT_TABLE.update(ContextUnpickler(fd).load())
         except EOFError:
             return
 
@@ -355,7 +369,10 @@ def load(profile):
 
         for ctxlist in _CONTEXT_TABLE.values():
             for ctx in ctxlist:
-                if not ctx.isVersionCompatible():
+                # Destroy the context in case of incompatibility or import failure.
+                # Check the alert_on_expire option because it can contain
+                # some external reference that will be called from the core.
+                if not ctx.isVersionCompatible() or ctx.getOptions()["alert_on_expire"] is _Dummy:
                     ctx.destroy()
 
 def wakeup(now):
